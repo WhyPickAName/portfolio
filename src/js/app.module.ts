da23b880d2742f207204763100e83ba93908d893
James Scales <james.scales@airsweb.com>
2020-11-29 23:40:02 +0000

    bulma implementation

--- src/js/app.module.ts
+++ src/js/app.module.ts
@@ -2,17 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer/footer.component';
-import { HeaderComponent } from './components/header/header/header.component';
-import { MenuComponent } from './components/menu/menu/menu.component';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    MenuComponent
   ],
   imports: [
     BrowserModule,
